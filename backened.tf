d65a0a631a2584111b300adc1ecfc79845764851
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2021-03-18 21:55:54 -0400

    remove profile

--- backened.tf
+++ backened.tf
@@ -3,7 +3,5 @@ terraform {
    bucket                   = "aboi"
    key                      = "terraform/lambda/terraform_lambda.tfstate"
    region                   = "us-east-1"
-   profile                  = "abasa"
-   shared_credentials_file  = "~/.aws/credentials"
  }
 }
