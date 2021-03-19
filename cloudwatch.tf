51d2b7f40e75d3efb23560d2bf47bf38c588e382
Akash Basa <39308879+aakashbasa@users.noreply.github.com>
2021-03-18 22:10:25 -0400

    add input for cloudwatch target

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,11 +1,12 @@
 resource "aws_cloudwatch_event_rule" "every_day" {
-  name                = "stop-rds"
-  description         = "Fires every day to stop rds"
-  schedule_expression = "rate(1 day)"
+  name                  = "stop-rds"
+  description           = "Fires every day to stop rds"
+  schedule_expression   = "rate(1 day)"
 }
 
 resource "aws_cloudwatch_event_target" "check_every_day" {
-  rule      = "${aws_cloudwatch_event_rule.every_day.name}"
-  target_id = "lambda"
-  arn       = "${aws_lambda_function.rds_lambda.arn}"
+  rule        = "${aws_cloudwatch_event_rule.every_day.name}"
+  target_id   = "lambda"
+  arn         = "${aws_lambda_function.rds_lambda.arn}"
+  input       = "{ “action”: “stop”, “dbInstance”: “rds-name” }"
 }
